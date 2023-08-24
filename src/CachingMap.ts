f57db96cf36ebd866c4f903872cac8d92ef8d896
Josh Goldberg <git@joshuakgoldberg.com>
2023-08-24 11:46:50 -0400

    feat: fetch co-authors by email

---
+++ src/CachingMap.ts
@@ -0,0 +1,21 @@
+export type Getter<Key, Value> = (key: Key) => Value;
+
+export class CachingMap<Key, Value> {
+	#cache = new Map<Key, Value>();
+	#getter: Getter<Key, Value>;
+
+	constructor(getter: Getter<Key, Value>) {
+		this.#getter = getter;
+	}
+
+	get(key: Key) {
+		const existing = this.#cache.get(key);
+		if (existing) {
+			return existing;
+		}
+
+		const value = this.#getter(key);
+		this.#cache.set(key, value);
+		return value;
+	}
+}
