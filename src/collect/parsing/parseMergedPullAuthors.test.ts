0619cc1f9f2ee5bd0473c9c28e91fa205bab47fd
Josh Goldberg <git@joshuakgoldberg.com>
2023-11-24 17:19:40 +0100

    fix: use commit-to-co-authors and co-author-to-username

--- src/collect/parsing/parseMergedPullAuthors.test.ts
+++ src/collect/parsing/parseMergedPullAuthors.test.ts
@@ -1,54 +1,36 @@
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 
-import { CachingMap } from "../../CachingMap.js";
 import { parseMergedPullAuthors } from "./parseMergedPullAuthors.js";
 
 describe("parseMergedPullAuthors", () => {
-	it.each([
-		[{ body: undefined, user: null }, []],
-		[{ body: undefined, user: { login: "abc123" } }, ["abc123"]],
-		[{ body: "feat: something", user: { login: "abc123" } }, ["abc123"]],
-		[
-			{
-				body: "feat: something\nco-authored-by: not-a-github-username",
-				user: { login: "abc123" },
-			},
-			["abc123"],
-		],
-		[
-			{
-				body: "feat: something\nco-authored-by: @def456",
-				user: { login: "abc123" },
-			},
-			["abc123", "def456"],
-		],
-		[
-			{
-				body: "feat: something\nco-authored-by: @def456 <ghi@jkl.com>",
-				user: { login: "abc123" },
-			},
-			["abc123", "def456"],
-		],
-		[
-			{
-				body: "feat: something\nco-authored-by: Username <def456@ghi.com>",
-				user: { login: "abc123" },
-			},
-			["abc123", "def456"],
-		],
-		[
-			{
-				body: "feat: something\nco-authored-by: Username <def456@ghi.com>\nco-authored-by: Username <def456@ghi.com>",
-				user: { login: "abc123" },
-			},
-			["abc123", "def456"],
-		],
-	])("given %j, returns %j", async (mergedPull, expected) => {
-		const result = await parseMergedPullAuthors(
-			mergedPull,
-			new CachingMap((key: string) => Promise.resolve(key.split("@")[0])),
+	it("returns an empty array when the pull has no user", async () => {
+		const actual = await parseMergedPullAuthors({}, vi.fn());
+
+		expect(actual).toEqual([]);
+	});
+
+	it("returns an empty array when the pull has a user with no login", async () => {
+		const actual = await parseMergedPullAuthors({ user: {} }, vi.fn());
+
+		expect(actual).toEqual([]);
+	});
+
+	it("returns the user login when the pull has a user with login", async () => {
+		const login = "abc123";
+
+		const actual = await parseMergedPullAuthors({ user: { login } }, vi.fn());
+
+		expect(actual).toEqual([login]);
+	});
+
+	it("retrieves usernames when the PR has a body with co-authors", async () => {
+		const username = "abc123";
+		const cachingCoAuthorToUsername = vi.fn().mockResolvedValue(username);
+		const actual = await parseMergedPullAuthors(
+			{ body: "co-authored-by: ..." },
+			cachingCoAuthorToUsername,
 		);
 
-		expect(result).toEqual(expected);
+		expect(actual).toEqual([username]);
 	});
 });
